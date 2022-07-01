8a4c0be13244dcba8de85cedf44d1eaa93fa9639
Opeyemi Ogunbanjo <theoseed@gmail.com>
2022-07-01 02:21:38 -0700

    Fix betty styling in C files 4 and 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+
+/**
+ * main - Prints text with puts
+ */
 int main(void)
 {
 	char text[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(text);
-	return 0;
+	return (0);
 }
