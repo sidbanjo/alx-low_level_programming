5f04da5ff463199ed7b23514b89d9d96aa64fcd9
Opeyemi Ogunbanjo <theoseed@gmail.com>
2022-07-01 02:43:58 -0700

    Fix error in C file 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	long long int lo;
 	float f;
 
-	printf("Size of a char: %u byte(s)\n" sizeof(char));
-	printf("Size of an int: %u byte(s)\n" sizeof(int));
-	printf("Size of a long: %u byte(s)\n" sizeof(long));
-	printf("Size of a long long: %u byte(s)\n" sizeof(long long));
-	printf("Size of a float: %u byte(s)\n" sizeof(float));
+	printf("Size of a char: %u byte(s)\n", (int)sizeof(c));
+	printf("Size of an int: %u byte(s)\n", (int)sizeof(i));
+	printf("Size of a long: %u byte(s)\n", (int)sizeof(l));
+	printf("Size of a long long: %u byte(s)\n", (int)sizeof(lo));
+	printf("Size of a float: %u byte(s)\n", (int)sizeof(f));
 	Return (0);
 }
