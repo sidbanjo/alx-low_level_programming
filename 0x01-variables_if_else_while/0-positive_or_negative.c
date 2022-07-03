f7bb69550b5b841a57748d89e177329b4ac26790
Opeyemi Ogunbanjo <theoseed@gmail.com>
2022-07-03 14:57:07 -0700

    Fix error in project 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-
+#include <stdio.h>
 /**
  * main - print sign of random number
  *
@@ -14,11 +14,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if n < 0
+	if (n < 0)
 		printf("%d is negative\n", n);
-	else if n == 0
+	else if (n == 0)
 		printf("%d is zero\n", n);
 	else
-		printf("%d is positive\n", n)
+		printf("%d is positive\n", n);
 	return (0);
 }
