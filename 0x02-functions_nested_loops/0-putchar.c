ec475adf7b717fca692f26813ffc448790f5a42e
Opeyemi Ogunbanjo <theoseed@gmail.com>
2022-07-06 14:06:09 -0700

    Fix test 0 ver3

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,6 +8,13 @@
  */
 int main(void)
 {
-	_putchar((char) *"_putchar\n");
+	char c[8] = "_putchar";
+	int i;
+	
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
