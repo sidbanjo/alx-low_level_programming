0248cd1469f504c1ab61a9b2508ba34d0352f485
Opeyemi Ogunbanjo <theoseed@gmail.com>
2022-07-06 11:01:17 -0700

    Add task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "0-putchar.h"
+
+/**
+ * main - prints "_putchar"
+ *
+ * Return: 0 to exit program
+ */
+int main(void)
+{
+	_putchar((char) *"_putchar\n");
+	return (0);
+}
