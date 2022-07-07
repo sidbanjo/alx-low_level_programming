1b92cb7e37cbce05c9f5453e20b09c53ec8c5299
Opeyemi Ogunbanjo <theoseed@gmail.com>
2022-07-06 18:03:17 -0700

    Add task 5

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,20 +1,6 @@
-/**
- * _putchar - prints to stdout
- */
 int _putchar(char);
-
-/**
- * print_alphabet - generates the alphabets
- */
 void print_alphabet(void);
-
-/**
- * print_alphabet_x10 - generates the alphabets 10 times
- */
 void print_alphabet_x10(void);
-
-/**
- * _islower - checks for lower case
- */
 int _islower(int c);
 int _isalpha(int c);
+int print_sign(int n);
