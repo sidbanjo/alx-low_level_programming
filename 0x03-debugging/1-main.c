752b68d8763fecf16fb01acf657263dba3d3081f
Opeyemi Ogunbanjo <theoseed@gmail.com>
2022-07-07 10:00:32 -0700

    Fix logic in task2

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,12 +11,12 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 
-	/** i = 0;
+	i = 0;
 
-	while (i < 10)
+	/* while (i < 10) - has no increment for @i */
 	{
 		putchar(i);
-	} */
+	}
 
 	printf("Infinite loop avoided! \\o/\n");
 
