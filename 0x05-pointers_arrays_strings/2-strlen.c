3ad3f5c09d9c244688dcb65a8eea45bf49cf811e
Opeyemi Ogunbanjo <theoseed@gmail.com>
2022-07-12 18:10:20 -0700

    Add task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen - checks length of a string
+ * @s: pointer to string
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define MAIN
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 int _putchar(char);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
