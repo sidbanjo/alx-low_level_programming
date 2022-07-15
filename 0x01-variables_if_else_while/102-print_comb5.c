872d53aa7c9bcedba77b58900fc39842fa2a2c41
Opeyemi Ogunbanjo <theoseed@gmail.com>
2022-07-15 12:18:40 -0700

    Finished project 0x01

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,32 +7,45 @@
  */
 int main(void)
 {
-        int a;
-        int b;
+	int a;
+	int b;
 	int c;
 	int d;
 
-        for (a = '0'; a <= '9'; a++)
-        {
-                for (b = '0'; b <= '9'; b++)
-                {
-			for (c = '0'; c <= '9'; c++)
+	for (a = '0'; a <= '9'; a++)
+	{
+	for (b = '0'; b <= '9'; b++)
+	{
+	for (c = '0'; c <= '9'; c++)
+	{
+	for (d = '1'; d <= '9'; d++)
+	{
+		if (!(a == b == c == d))
+		{
+		if (!(a == c && b == d))
+		{
+		if (c >= a)
+		{
+		if (d == 0 || d > b)
+		{
+			putchar(a);
+			putchar(b);
+			putchar(' ');
+			putchar(c);
+			putchar(d);
+			if (!(a == '8' && b == '9'))
 			{
-				for (d = '1'; d <= '9'; d++)
-				{
-					if (!(a == b == c == d))
-
-                                putchar(a);
-                                putchar(b);
-                                if (!(a == '8' && b == '9'))
-                                
-                                        putchar(',');
-                                        putchar(' ');
-                                }
-
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		}
+		}
+		}
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
 }
