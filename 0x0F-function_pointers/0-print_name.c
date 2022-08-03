4414c7d6988f1a6d24d0ddffa929b28f6fb85527
Opeyemi Ogunbanjo <theoseed@gmail.com>
2022-08-03 07:24:30 -0700

    Fix task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,5 @@
+#include "function_pointers.h"
+
 /**
  * print_name - prints a name
  * @name: name to print
@@ -7,5 +9,11 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name == NULL)
+	{
+	}
+	else
+	{
+		f(name);
+	}
 }
