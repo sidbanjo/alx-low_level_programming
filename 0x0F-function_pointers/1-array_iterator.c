e7ea0068dc10d63258ac9e3269a450be67437ffb
Opeyemi Ogunbanjo <theoseed@gmail.com>
2022-08-03 20:28:35 -0700

    Fix task 1 2

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,8 +15,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array == NULL || size == 0)
 	{
 	}
-	for (i = 0; i < size; i++)
+	else
 	{
-		action(array[i]);
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
 	}
 }
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,12 +12,18 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || size <= 0)
+	if (size <= 0)
 		return (-1);
-	for (i = 0; i < size; i++)
+	if (array == NULL)
 	{
-	if (cmp(array[i]))
-		return (i);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
 	}
 	return (-1);
 }
