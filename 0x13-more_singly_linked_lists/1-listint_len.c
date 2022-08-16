62f89e6be769d52a4d7b3474d9271926fecadacd
Opeyemi Ogunbanjo <theoseed@gmail.com>
2022-08-16 11:32:29 -0700

    Fix error in task 1 9

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,22 +1,17 @@
 #include "lists.h"
 
 /**
- * print_listint - Prints all elements of a list.
+ * listint_len - Returns number of elements in a list.
  * @h: head of the linked list
  *
  * Return: number of nodes
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t n = 0;
 
-	if (h == NULL)
-	{
-		return (0);
-	}
 	while (h != NULL)
 	{
-		printf("%i\n", h->n);
 		h = h->next;
 		n++;
 	}
